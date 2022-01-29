2aa9f304e2066e7fdc156cbe6c28c79e24b3fd78
youssif hany <youssifhany75@gmail.com>
2022-01-29 22:33:14 +0200

    configure esbuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,51 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">Hallo</div>;
+  const ref = useRef<any>();
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  async function submitHandler() {
+    if (!ref.current) return;
+
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+
+    // console.log(result);
+    setCode(result.outputFiles[0].text);
+  }
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={submitHandler}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 }
 
 export default App;
