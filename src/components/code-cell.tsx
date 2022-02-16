10d72f9a0167a06d0943555b6c8f72ef903d5b9c
youssif hany <youssifhany75@gmail.com>
2022-02-16 23:22:52 +0200

    add debouncing

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import bundle from "../bundler";
 import CodeEditor from "../components/code-editor";
@@ -9,10 +9,14 @@ function CodeCell() {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  async function submitHandler() {
-    const output = await bundle(input);
-    setCode(output);
-  }
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [input]);
 
   return (
     <Resizable direction="vertical">
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -32,7 +32,10 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   const iframe = useRef<any>();
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, "*");
+
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, "*");
+    }, 50);
   }, [code]);
 
   return (
